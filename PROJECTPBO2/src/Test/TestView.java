664f94e0ef63a7f301a8b0489f8cb671ccdc83f7
jarkom <jarkom@jarkom-C00>
2018-09-07 12:34:00 +0700

    versi baru

---
+++ PROJECTPBO2/src/Test/TestView.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Test;
+
+import View.DaftarAntrianDialog;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author admin
+ */
+public class TestView {
+    public static void main(String[] args) {
+        DaftarAntrianDialog test = new DaftarAntrianDialog();
+        test.setSize(500,800);
+        test.setTitle("Daftar Antrian Pasien");
+        test.setVisible(true);
+        
+        
+    }
+}
